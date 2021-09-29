d584b42323bdb4e35899e71e810e148a6912b331
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-29 21:32:13 +0200

    api

--- service/ApiHandler.js
+++ service/ApiHandler.js
@@ -7,10 +7,12 @@ class ColorApi {
       baseURL: 'https://www.thecolorapi.com'
     });
   }
-
+   //colores simples + hexadecimal del color
   getColor = (color) => this.api.get(`/scheme/?hex=${color}&mode=analogic&count=1`);
-  getGradientScheme =(color) => this.api.get(`/scheme/?hex=${color}&mode=analogic&count=3`);
   getHexScheme =(hex)=> this.api.get(`/scheme/?hex=${hex.value}`);
+  //Combinaciones de 3 colores + numero hexadecimal de los 3 colores
+  getGradientScheme =(color) => this.api.get(`/scheme/?hex=${color}&mode=analogic&count=3`);
+  getHexGradientScheme =(hex)=> this.api.get (`/scheme/?hex=${hex.value}&mode=analogic&count=3`)
 
 }
 
