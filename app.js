97b9257251cbf75f5675facd5a93edd2c40e506a
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-29 12:19:22 +0200

    new comit

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ require("./config")(app);
 
 
 // 👇 MIDDLEWARE MISSING
+const authRouter = require("./routes/auth.routes");
+app.use("/api/auth", authRouter);
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
-const authRouter = require("./routes/auth.routes");
-app.use("/api/auth", authRouter);
+
 
 const projectRouter = require("./routes/project.routes");
 app.use("/api", isAuthenticated, projectRouter);            // <== UPDATE
@@ -23,11 +24,11 @@ const taskRouter = require("./routes/task.routes");
 app.use("/api",isAuthenticated, taskRouter);            // <== UPDATE
 
 
-app.use((req, res, next) => {
+//app.use((req, res, next) => {
     // If no routes match, send them the React HTML.
-    res.sendFile(__dirname + "/public/index.html");
-  });
+  //  res.sendFile(__dirname + "/public/index.html");
+//  });
 
-// require("./error-handling")(app);
+require("./error-handling")(app);
 
 module.exports = app;
